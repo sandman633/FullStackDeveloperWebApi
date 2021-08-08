7471a664e6f1579cc95fdbeb7eadc9fdb4f2f001
sandman633 <69395587+sandman633@users.noreply.github.com>
2021-08-08 19:42:23 +0300

    added default infrastructure

---
+++ FullStackDeveloperWebApi/Models/UserActivity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FullStackDeveloperWebApi.Models
+{
+    public class UserActivity
+    {
+        public int Id { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public DateTime LastActivityDate { get; set; }
+
+    }
+}
---
+++ FullStackDeveloperWebApi/Services/UserActivityService.cs
@@ -0,0 +1,21 @@
+﻿using FullStackDeveloperWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FullStackDeveloperWebApi.Services
+{
+    public class UserActivityService
+    {
+        private readonly Context _context;
+
+        public UserActivityService(Context context)
+        {
+            _context = context;
+        }
+
+
+
+    }
+}
